408b8067eff88db5e17e4e8daef3611406090b94
JEREMY FAGGION <jeremy.faggion@etu.univ-lyon.fr>
2019-11-08 17:26:49 +0100

    ajout du dossier assets pour images et effets sonores; ajout de detectPlug.py

---
+++ detectPlug.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+def detectPlug():
+    oldList = []
+    while True:
+        newList = os.popen("lsusb | awk '{print $6}'").read().split("\n")[:-1]
+        
+        if len(oldList) == 0:
+            pass
+        elif len(newList)>len(oldList):
+            os.popen("machin.mp3")
+        elif len(newList)<len(oldList):
+            print("REMOVED")
+        
+        oldList = newList
+        time.sleep(.2)
+
+detectPlug()
